4f8bf8fd98a9b38c4e6cab347f1cd345ef69b2c0
miloszKubiak <milypan69@gmail.com>
2022-07-02 12:31:21 +0200

    setup project

---
+++ src/context/context.js
@@ -0,0 +1,16 @@
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
+
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+	return <AppContext.Provider value="test">{children}</AppContext.Provider>;
+};
+
+
+export const useGlobalContext = () => {
+	return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
