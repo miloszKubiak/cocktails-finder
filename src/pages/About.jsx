513b89cbff09406462a2d58c508a0e662ab07b35
miloszKubiak <milypan69@gmail.com>
2022-07-02 13:51:14 +0200

    create and style error page and about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem odit voluptatum necessitatibus asperiores consequatur assumenda officiis iure inventore esse dolores commodi officia tenetur nesciunt rerum, possimus modi veritatis ut numquam?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo tenetur ducimus rerum, facilis nihil blanditiis totam perspiciatis distinctio suscipit.
+      </p>
+    </section>
   )
 }
 
