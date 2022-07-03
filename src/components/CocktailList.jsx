b2c1a37be809d4147392f0dd6b23335af985e8d9
miloszKubiak <milypan69@gmail.com>
2022-07-03 12:01:22 +0200

    create fetch function

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -13,7 +13,7 @@ const CocktailList = () => {
 	if (cocktails.length < 1) {
 		return (
 			<h2 className="section-title">
-				no cocktails matched your search criteria :(
+				No cocktails matched your search criteria :(
 			</h2>
 		);
 	}
